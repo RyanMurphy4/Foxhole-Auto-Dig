b7024c27c5199587e4cf93aa13399026bf3ffa37
DeontayPeterson <108695175+DeontayPeterson@users.noreply.github.com>
2022-08-31 02:09:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,35 @@
+import time
+from math import sqrt
+
+import keyboard
 import torch
 import numpy as np
 import pandas
 import multiprocessing as mp
 from finder import Finder
 from window_capture import Screencap
-from test_data import Character
+from character import Character
+
 
 cap = Screencap()
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_seg_.938.pt')
 mike = Character()
 
+def detect_bp():
 
-def detect_posts():
-
-    post_xy_locations = []
     screen_shot = cap.take_screenshot()
     result = model(screen_shot)
     locations = result.pandas().xyxy[0]
     locations_list = list(locations.iterrows())
-    return locations_list
-
-
-def get_post_coords(location_list):
-    center_xy_list = []
-    for location in location_list:
-        x_min = location[1][0]
-        y_min = location[1][1]
-        x_max = location[1][2]
-        y_max = location[1][3]
-        center_x = ((x_max - x_min) / 2) + x_min
-        center_y = ((y_max - y_min) / 2) + y_min
-        post_confidence = location[1][4]
-
-
-        center_xy_list.append((center_x, center_y))
-
-    return center_xy_list
-
-def center_coords(return_dictionary):
-    temp_location_list = detect_posts()
-    temp_center_list = get_post_coords(temp_location_list)
-
-    return_dictionary['temp_center_list'] = temp_center_list
+    mike.bp_locations = locations_list
 
 if __name__ == '__main__':
-    manager = mp.Manager()
-    return_dictionary = manager.dict()
-    the_process = mp.Process(target=center_coords, args=(return_dictionary,))
-    the_process.run()
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='best_seg_.938.pt')
+    model.conf = .90
+    # climb_process = mp.Process(target=mike.always_climb)  # Commented out for testing. Un-comment these two lines when testing movement.
+    # climb_process.start()
 
-    print(return_dictionary.values())
+    mike.position_mouse()
+
+    while True:
+        detect_bp()
+        print(mike.bp_locations)
