4caadf78cb4b76403206aabd3510e932c0e027b0
DeontayPeterson <108695175+DeontayPeterson@users.noreply.github.com>
2022-08-31 02:09:56 -0400

    Delete test_data.py

--- test_data.py
+++
@@ -1,7 +0,0 @@
-class Character:
-    def __init__(self):
-        self.post_location_list = ''
-
-    def retrieve_location_list(self):
-        print(self.post_location_list)
-        return self.post_location_list
